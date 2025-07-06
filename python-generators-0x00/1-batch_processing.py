71de30c59e287c86a2f576a80d277360e02c34fb
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-06 21:08:21 +0000

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,1 +1,43 @@
+import mysql.connector
+
+def stream_users_in_batches(batch_size):
+    """
+    Generator to yield batches of rows from the user_data table
+    """
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='aswise?Ben10',  # change your password
+        database='ALX_prodev'
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM user_data")
+
+    batch = []
+    for row in cursor:   # loop 1
+        batch.append(row)
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+    if batch:
+        yield batch
+
+    cursor.close()
+    connection.close()
+
+
+def batch_processing(batch_size):
+    """
+    Processes batches to yield users over age 25
+    """
+    for batch in stream_users_in_batches(batch_size):   # loop 2
+        for user in batch:  # loop 3
+            if user[3] > 25:
+                yield user
+
+
+# test
+if __name__ == "__main__":
+    for user in batch_processing(5):
+        print(user)
 
