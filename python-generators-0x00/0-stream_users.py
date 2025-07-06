bd8f2f739df37fe4c940280bb6e36db285abfab5
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-06 21:02:44 +0000

    Update 0-stream_users.py

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,1 +1,22 @@
+import mysql.connector
+
+def stream_users():
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='aswise?Ben10',  # change this to your actual password
+        database='ALX_prodev'
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM user_data")
+
+    for row in cursor:
+        yield row
+
+    cursor.close()
+    connection.close()
+
+if __name__ == "__main__":
+    for user in stream_users():
+        print(user)
 
