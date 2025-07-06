66183e1867c7d8572ed225d19b0598dda82ced29
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-06 21:27:21 +0000

    Update 4-stream_ages.py

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -1,1 +1,41 @@
+import mysql.connector
+
+def stream_user_ages():
+    """
+    Generator to yield user ages one by one
+    """
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='aswiseBen10',  # change to your password
+        database='ALX_prodev'
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT age FROM user_data")
+
+    for row in cursor:   # loop 1
+        yield row[0]
+
+    cursor.close()
+    connection.close()
+
+
+def calculate_average_age():
+    """
+    Calculate the average age using the generator
+    """
+    total = 0
+    count = 0
+    for age in stream_user_ages():  # loop 2
+        total += age
+        count += 1
+    if count == 0:
+        print("No users found")
+    else:
+        average = total / count
+        print(f"Average age of users: {average:.2f}")
+
+# test
+if __name__ == "__main__":
+    calculate_average_age()
 
