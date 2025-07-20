730354c9b6d1f5b230f570152f2c717235e52211
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-20 14:32:59 +0000

    Update test_clients.py

--- 0x03-Unittests_and_integration_tests/test_clients.py
+++ 0x03-Unittests_and_integration_tests/test_clients.py
@@ -66,5 +66,5 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         self.assertEqual(client.public_repos(), self.expected_repos)
     def test_public_repos_with_license(self):
         client = GithubOrgClient("google")
+        
         self.assertEqual(client.public_repos(license='apache-2.0'), self.apache2_repos)
-
