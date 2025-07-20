aff241590685f1688c3fbb488301895422c326a1
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-20 22:35:56 +0000

    Update models.py

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,1 +1,22 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+class User(AbstractUser):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    ROLE_CHOICES = (('guest', 'Guest'), ('host', 'Host'), ('admin', 'Admin'))
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='guest')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(User, related_name="conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    conversation = models.ForeignKey(Conversation, related_name="messages", on_delete=models.CASCADE)
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
