c35297e7e700518360a0524231ec0ecfc2cbcde5
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-26 10:17:02 +0000

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,9 +1,11 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsParticipant(BasePermission):
+
+class IsParticipant(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user in obj.participants.all()
 
-class IsSender(BasePermission):
+
+class IsSender(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.sender == request.user
+        return obj.sender == request.user  # Fixed typo here
