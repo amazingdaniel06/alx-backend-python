7c691148797e983fb74a136a65bdf1e2650ef92e
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-07-26 10:30:14 +0000

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -70,9 +70,6 @@ AUTH_USER_MODEL = 'chats.User'
 
 CORS_ALLOW_ALL_ORIGINS = True
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
