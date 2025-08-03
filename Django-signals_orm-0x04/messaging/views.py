6b29e97d22031745ce0c9773eaac8a5c067e66f4
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-08-03 17:34:07 +0000

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,1 +1,10 @@
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 
+def delete_user(request, user_id):
+    try:
+        user = User.objects.get(pk=user_id)
+        user.delete()
+        return JsonResponse({'message': 'User deleted successfully'})
+    except User.DoesNotExist:
+        return JsonResponse({'error': 'User not found'}, status=404)
